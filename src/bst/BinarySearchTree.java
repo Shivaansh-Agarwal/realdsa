c243e0d424dde47c2bedee45ea847663386a93f0
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2025-11-30 13:57:41 +0530

    bst changes

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -129,6 +129,39 @@ class BinarySearchTree {
         return false;
     }
 
+    public void delete(int x) {
+        deleteNodeRecursive(root, x);
+    }
+
+    private Node deleteNodeRecursive(Node root, int x) {
+        if (root == null) {
+            return null;
+        }
+        if (root.key > x) {
+            root.left = deleteNodeRecursive(root.left, x);
+        } else if (root.key < x) {
+            root.right = deleteNodeRecursive(root.right, x);
+        } else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                Node successor = getSuccessor(root);
+                root.key = successor.key;
+                root.right = deleteNodeRecursive(root.right, successor.key);
+            }
+            return root;
+        }
+        return root;
+    }
+
+    private Node getSuccessor(Node root) {
+        Node curr = root.right;
+        while (curr != null && curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+
 }
 
 enum TraversalTypeEnum {
