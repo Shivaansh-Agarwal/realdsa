84d8e83fe7ea1646f239a5a11319d309ba04c6f5
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2025-12-13 21:05:17 +0530

    heap delete operations

--- src/heaps/MaxHeap.java
+++ src/heaps/MaxHeap.java
@@ -25,6 +25,40 @@ public class MaxHeap {
         }
     }
 
+    public void delete(int value) {
+        int index = -1;
+        for (int i=0; i<heap.size(); i++) {
+            if (heap.get(i) == value) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            return;
+        }
+        heap.set(index, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        while (true) {
+            int leftChild = 2*index + 1;
+            int rightChild = 2*index + 2;
+            int largest = index;
+            if (leftChild < heap.size() && heap.get(leftChild) > heap.get(largest)) {
+                largest = leftChild;
+            }
+            if (rightChild < heap.size() && heap.get(rightChild) > heap.get(largest)) {
+                largest = rightChild;
+            }
+            if (largest != index) {
+                int temp = heap.get(index);
+                heap.set(index, heap.get(largest));
+                heap.set(largest, temp);
+                index = largest;
+            } else {
+                break;
+            }
+        }
+    }
+
     public void print() {
         for (int i=0; i<heap.size(); i++) {
             System.out.print(heap.get(i) + " ");
--- src/heaps/Solution.java
+++ src/heaps/Solution.java
@@ -1,11 +1,16 @@
 public class Solution {
     public static void main(String[] args) {
         MaxHeap maxHeap = new MaxHeap();
-        int[] values = { 10, 7, 11, 5, 4, 13 };
+        int[] values = { 13, 16, 31, 41, 51, 100 };
         for (int value : values) {
             maxHeap.insert(value);
-            System.out.print("Inserted " + value + " into the max-heap: ");
-            maxHeap.print();
         }
+
+        System.out.print("Initial heap: ");
+        maxHeap.print();
+
+        maxHeap.delete(13);
+        System.out.print("heap after deleting 13: ");
+        maxHeap.print();
     }
 }
